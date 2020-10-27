f4a9baa8dd29340cd9c15513c79af6e342fd9998
bonefraktur <73370902+bonefraktur@users.noreply.github.com>
2020-10-27 01:56:01 +0000

    Update !.lua

--- !.lua
+++ !.lua
@@ -1,3 +1,14 @@
+local function IsAlive()
+  local Value = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("MainGUI"):WaitForChild("Game"):WaitForChild("CashBag").Visible
+
+  if Value then return true else if not Value then return false end end
+
+end
+
+repeat 
+  wait()
+until IsAlive()
+
 syn.queue_on_teleport("wait(10) loadstring(game:HttpGet('https://raw.githubusercontent.com/bonefraktur/Scripts/main/%3F.lua'))()")
 wait(180)
 game:GetService("TeleportService"):Teleport(game.PlaceId)
