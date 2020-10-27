1632a7cee5b379d56ae57000f17b64bf23d79119
bonefraktur <73370902+bonefraktur@users.noreply.github.com>
2020-10-27 01:53:59 +0000

    Update !.lua

--- !.lua
+++ !.lua
@@ -1,3 +1,3 @@
-  wait(180)
-  syn.queue_on_teleport("wait(10) loadstring(game:HttpGet('https://raw.githubusercontent.com/bonefraktur/Scripts/main/%3F.lua'))()")
-  game:GetService("TeleportService"):Teleport(game.PlaceId)
+syn.queue_on_teleport("wait(10) loadstring(game:HttpGet('https://raw.githubusercontent.com/bonefraktur/Scripts/main/%3F.lua'))()")
+wait(180)
+game:GetService("TeleportService"):Teleport(game.PlaceId)
