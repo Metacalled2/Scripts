670e328b8fcd98de85a68a4b7299ff1b69d44f79
bonefraktur <73370902+bonefraktur@users.noreply.github.com>
2020-10-27 01:56:34 +0000

    Update !.lua

--- !.lua
+++ !.lua
@@ -7,8 +7,11 @@ end
 
 repeat 
   wait()
+   warn('YH OK IM WAITING TILL UR ALIVE FOR THE SET TELEPORT')
 until IsAlive()
 
 syn.queue_on_teleport("wait(10) loadstring(game:HttpGet('https://raw.githubusercontent.com/bonefraktur/Scripts/main/%3F.lua'))()")
+warn('TELEPORT SET MY GUY!')
+print('LOL SET THE TP MY GUY [2] !')
 wait(180)
 game:GetService("TeleportService"):Teleport(game.PlaceId)
